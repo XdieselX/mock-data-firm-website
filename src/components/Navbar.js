85b1eccaf6ab39a4d394e6869e7416ab1d0a8dbc
Ash Fallon <ashfallon23@gmail.com>
2022-06-13 14:20:11 +0100

    Added state to navbar for hamburger menu functionality, added styling changes to desktop menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,38 @@
+import { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const navClickHandler = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className='max-w-[1240px] px-4 flex justify-between items-center h-24 mx-auto bg-transparent text-white'>
       {/* Navbar title - desktop */}
       <h1 className='w-full text-3xl font-bold text-[#00df9a] uppercase'>
         Surge Analytics.
       </h1>
-      <ul className='hidden flex space-x-8'>
+      <ul className='hidden md:flex space-x-8'>
         {/* Navbar list - desktop */}
-        <li>Home</li>
-        <li>Company</li>
-        <li>Resources</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer'>Company</li>
+        <li className='cursor-pointer'>Resources</li>
+        <li className='cursor-pointer'>About</li>
+        <li className='cursor-pointer'>Contact</li>
       </ul>
-      <div>
+      <div onClick={navClickHandler} className='block md:hidden'>
         {/* Hamburger menu */}
-        <AiOutlineMenu size={20} />
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 px-4 bg-[#000300]'>
+      <div
+        className={
+          !nav
+            ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 px-4 bg-[#000300] ease-in-out duration-500'
+            : 'fixed left-[-100%]'
+        }
+      >
         {/* Navbar title - mobile */}
         <h1 className='w-full m-4 text-3xl font-bold text-[#00df9a] uppercase'>
           Surge Analytics.
